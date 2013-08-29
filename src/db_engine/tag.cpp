0b8739f875a0cba5fd09c884b7a6fa9396cecee0
Jakub Liput <jakub.liput@gmail.com>
2013-08-29 17:15:51 +0200

    Adding GPL3 header to each source file

--- src/db_engine/tag.cpp
+++ src/db_engine/tag.cpp
@@ -1,3 +1,21 @@
+/****************************************************************************
+* Slicepod
+* Copyright (C) 2013 Jakub Liput <jakub.liput@gmail.com>
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*****************************************************************************/ 
+
 #include "tag.hpp"
 #include "fragment.hpp" // because reference is used
 #include "fragmenttagmap.hpp" // because of fragments list getter
